9da139dc371715ac87e6b0071560d3353c1568c5
AsadCh001 <asadrizwan001@gmail.com>
2024-03-27 21:10:46 +0500

    check

--- app/url.py
+++ app/url.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from app import views
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 from .views import RecommendedLearningPathsView
 
 
